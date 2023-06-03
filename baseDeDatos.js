4f081a38a8fc36fa78d0e2fe9d3f68b5c532b02d
Scarlethev <scarleth_07@hotmail.com>
2023-06-03 17:18:07 -0300

    usando base de datos

--- baseDeDatos.js
+++ baseDeDatos.js
@@ -3,81 +3,72 @@ var dataGym = {
     "productos": [
       {
         "id": 1,
-        "image": "imagenes/cuerdasParaSaltar.JPG",
+        "imagen": "imagenes/cuerdasParaSaltar.JPG",
         "titulo": "Soga",
         "categoria" : "aerobico",
-        "description": "Esta cuerda para saltar hará que mejores tu fuerza, resistencia, agilidad y coordinación con probablemente la mejor comba en el mercado. Es de gran calidad y duradera para la máxima satisfacción y seguridad durante el entrenamiento.",
-        "price": 5450
+        "descripcion": "Esta cuerda para saltar hará que mejores tu fuerza, resistencia, agilidad y coordinación con probablemente la mejor comba en el mercado. Es de gran calidad y duradera para la máxima satisfacción y seguridad durante el entrenamiento.",
+        "precio": 5450
       },
       
       {
         "id": 2,
-        "image": "imagenes/man-g77843936b_1280.jpg",
+        "imagen":"imagenes/kettlebell-g897bdaea6_1280.jpg",
         "titulo": "Pesas 3KG",
         "categoria" : "Pesas",
-        "description": "",
-        "price": 5700 
+        "descripcion": "",
+        "precio": 5700 
       },
 
       {
         "id": 3,
-        "image": "imagenes/suplemento.JPG",
+        "imagen": "imagenes/suplemento.JPG",
         "titulo": "whey Protein",
         "categoria" : "suplementos",
-        "description": "",
-        "price": 6600
+        "descripcion": "",
+        "precio": 6600
       },
 
       {
         "id": 4,
-        "image": "imagenes/termica-mc-51-fafefa47abe33e354b16729461259711-1024-1024.JPG",
+        "imagen": "imagenes/termica-mc-51-fafefa47abe33e354b16729461259711-1024-1024.JPG",
         "titulo": "Remera termica",
         "categoria" : "indumentaria",
-        "description": "",
-        "price": 18000
+        "descripcion": "",
+        "precio": 18000
       },
       {
         "id": 5,
-        "image": "imagenes/short deportivo.jpg",
+        "imagen":"imagenes/short.jpg",
         "titulo": "Short",
         "categoria" : "indumentaria",
-        "description": "",
-        "price": 20000
+        "descripcion": "",
+        "precio": 20000
       },
 
       {
         "id": 6,
-        "image": "imagenes\zapatillasDeportivas.JPG",
+        "imagen": "imagenes/zapatillasDeportivas.JPG",
         "titulo": "Zapatillas",
         "categoria" : "indumentaria",
-        "description": "Estas zapatillas reducen el esfuerzo de los músculos y de los tendones, tienen un buen agarre a las diferentes superficies del suelo en función del deporte.",
-        "price": 35000
+        "descripcion": "Estas zapatillas reducen el esfuerzo de los músculos y de los tendones, tienen un buen agarre a las diferentes superficies del suelo en función del deporte.",
+        "precio": 35000
       },
       {
         "id": 7,
-        "image":"imagenes\setPlataforma.JPG",
+        "imagen":"imagenes/setPlataforma.JPG",
         "titulo": "Set Plataforma",
         "categoria" : "aerobico",
-        "description": "Set Plataforma :CUSHION DE EQUILIBRIO en color azul ideal para la realización de ejercicios de propiocepción y equilibrio, fabricado en foam de alta densidad .Dimensiones: 48396 cm. Color: Azul real.",
-        "price": 16000
+        "descripcion": "Set Plataforma :CUSHION DE EQUILIBRIO en color azul ideal para la realización de ejercicios de propiocepción y equilibrio, fabricado en foam de alta densidad .Dimensiones: 48396 cm. Color: Azul real.",
+        "precio": 16000
       },
 
       {
         "id": 8,
-        "image":"imagenes\pesasRusas.JPG",
+        "imagen":"imagenes/pesasRusas.JPG",
         "titulo": "Pesas Relativas",
         "categoria" : "pesas",
-        "description": "PESAS RUSAS:  producto Pesas rusas confeccionada en plástico, rellena de cal, cemento y arena. Son una gran herramienta de entrenamiento funcional.",
-        "price": 8500
-      },
-
-      {
-        "id": 9,
-        "image":"imagenes\suplementoWhey.JPG",
-        "titulo": "Suplemento Whey",
-        "categoria" : "suplementos",
-        "description": "Contiene todos los aminoacidos esenciales y no esenciales ya que son naturales del concentrado de proteina de suero lacteo",
-        "price": 4320
+        "descripcion": "PESAS RUSAS:  producto Pesas rusas confeccionada en plástico, rellena de cal, cemento y arena. Son una gran herramienta de entrenamiento funcional.",
+        "precio": 8500
       }
     ]
     }
--- index.js
+++ index.js
@@ -1,1 +1,280 @@
 console.log(dataGym);
+let productos = (dataGym.productos)
+console.log(productos)
+let actividades = document.getElementById("galeria")
+let carousel = document.getElementsByClassName("carousel slide container-fluid")
+let tarjetas = document.getElementById("productos")
+console.log(tarjetas)
+console.log(actividades)
+console.log(carousel)
+
+let navbarLi = document.getElementsByClassName("navLink");
+console.log(navbarLi)
+
+for (let i = 0; i < navbarLi.length; i++) {
+  let element = navbarLi[i];
+  element.addEventListener("click", function (e) {
+    imprimir(e.target.id)
+  })
+}
+
+function imprimir(id) {
+  console.log("id imprimir", id)
+
+  switch (id) {
+
+    case "planes":
+      console.log("Estoy en planes");
+      planes.style.display = "flex",
+      actividades.style.display = "none",
+      containerDisciplinas.style.display = "none",
+      carouselExampleAutoplaying.style.display = "none",
+      pase.style.display= "none",
+      // tienda.style.display= "none",
+      // productos.style.display="none"
+      // row.style.display = "none",
+      containerContacto.style.display="none"
+      break;
+
+    case "tienda":
+      console.log(productos);
+      planes.style.display = "none",
+      actividades.style.display = "none",
+      containerDisciplinas.style.display = "none",
+      carouselExampleAutoplaying.style.display = "none",
+      pase.style.display= "none",
+      // tienda.style.display= "none",
+      // productos.style.display="none"
+      // row.style.display = "none",
+      containerContacto.style.display="none"
+      // tienda.style.display = "flex"
+      break;
+
+    case "sedes":
+      console.log("Estoy en sede");
+      planes.style.display = "flex",
+      actividades.style.display = "none",
+      containerDisciplinas.style.display = "none",
+      carouselExampleAutoplaying.style.display = "none",
+      pase.style.display= "none",
+      // tienda.style.display= "none",
+      // productos.style.display="none"
+      // row.style.display = "none",
+      containerContacto.style.display="none"
+      break;
+
+
+    case "contacto":
+      console.log("Estoy en contacto");
+      planes.style.display = "none",
+      actividades.style.display = "none",
+      containerDisciplinas.style.display = "none",
+      carouselExampleAutoplaying.style.display = "none",
+      pase.style.display= "none",
+      // tienda.style.display= "none",
+      // productos.style.display="none"
+      // row.style.display = "none",
+      containerContacto.style.display="flex"
+      break
+
+    case "login":
+      console.log("Estoy en login");
+      planes.style.display = "none",
+      actividades.style.display = "none",
+      containerDisciplinas.style.display = "none",
+      carouselExampleAutoplaying.style.display = "none",
+      pase.style.display= "none",
+      // tienda.style.display= "none",
+      // productos.style.display="none"
+      // row.style.display = "none",
+      containerContacto.style.display="none"
+      // login.style.display= "flex"
+      break;
+
+    default:
+      console.log("Estoy en login");
+      planes.style.display = "flex",
+      actividades.style.display = "flex",
+      containerDisciplinas.style.display = "flex",
+      carouselExampleAutoplaying.style.display = "flex",
+      pase.style.display= "flex",
+      // tienda.style.display= "flex",
+      // productos.style.display="flex"
+      // row.style.display = "flex",
+      containerContacto.style.display="flex"
+      // login.style.display= "flex"
+      categories(productos)
+      display(productos)
+      break;
+  }
+}
+
+function display(array) {
+
+  let html = "";
+  for (var i = 0; i < array.length; i++) {
+    html +=
+      `
+          <div class="col-lg-3 col-md-6 col-sm-6 d-flex">
+          <div class="card w-100 my-5 shadow-2-strong">
+           <img src="${array[i].imagen}" style="aspect-ratio: 1 / 1" />
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">${array[i].titulo}</h5>
+            <p class="card-text">$${array[i].precio}</p>
+            <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
+              <a href="#!" class="btn btn-danger shadow-0 me-1">Detalle</a>
+              <a href="#!" class="btn btn-light border px-2 pt-2 icon-hover"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
+            </div>
+          </div> 
+          </div>
+          </div>
+      `
+  }
+
+  tarjetas.innerHTML = html;
+
+}
+
+
+
+
+
+// FORMULARIO DE RESERVA- CAPTURE DE DATOS
+
+let form = document.querySelector("form")
+const modal = document.querySelector('#modal')
+const cerrar = document.querySelector('.close')
+form.addEventListener("submit", (event) => {
+  actionForm(event)
+  modal.style.display = "block"
+})
+
+function modalForm() {
+  modal.style.display = "none";
+  location.reload()
+}
+cerrar.addEventListener("click", modalForm);
+
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+})
+
+function actionForm(evento) {
+  evento.preventDefault()
+  let formDatos = {
+    nombreYapellido: evento.target[0].value,
+    direccionDeEmail: evento.target[1].value,
+    telefono: evento.target[2].value,
+    dni: evento.target[3].value
+  }
+  console.log(formDatos);
+}
+
+
+function backHome() {
+  setState("paginaANavegar", "index")
+  ChangeTemplateLayaout()
+}
+
+// CHECKBOX DINAMICAS
+
+//FUNCION SEARCH
+
+input.addEventListener("keyup", function (vino) {
+
+  var datoInput = vino.target.value;
+  let datoLimpio = datoInput.trim().toLowerCase();
+  console.log(datoLimpio)
+
+  //  let filtrado= arrayAFiltrar.filter(vino => vino.Nombre.toLowerCase().includes(datoLimpio))
+
+  // pintarHTML(filtrado)
+
+  filtrosCombinados()
+})
+
+
+
+//CREACION DE CHECKBOX DINAMICAS
+
+function categories(array) {
+  let categories = array.map(category => category.categoria)
+  let unica = new Set(categories)
+  let lastCategories = [...unica]
+
+
+  //FILTROS UVAS
+
+  let categorias = ""
+  lastCategories.map(category =>
+      categorias +=
+      `<div>
+      <input type="checkbox" value="${category}">
+      <label> ${category}</label>
+      <div>
+  `
+  )
+  document.getElementById("checkbox").innerHTML = categorias
+
+  checkboxListener()
+
+}
+
+
+// FUNCION DE FILTRADO CHECKBOX PAISES 
+
+function checkboxListener() {
+
+  var checkboxes= document.getElementById("checkbox");
+  var checkbox = checkboxes.querySelectorAll('input[type="checkbox"]');
+
+  for (var i = 0; i < checkbox.length; i++) {
+      checkbox[i].addEventListener("click", function () {
+          arrayCheckbox = [];
+          for (var i = 0; i < checkbox.length; i++) {
+              if (checkbox[i].checked) {
+                  arrayCheckbox.push(checkbox[i].value)
+              }
+          }
+          console.log(arrayCheckbox)
+          filtrosCombinados()
+      })
+  }
+}
+
+function filtrosCombinados() {
+
+  var vinosPorBanderas = [];
+
+  if (datoLimpio !== "" && arrayCheckbox.length > 0) {
+      arrayCheckbox.map(bandera => {
+          vinosPorBanderas.push(...arrayAFiltrar.filter(vino =>
+              vino.nombre.toLowerCase().includes(datoLimpio) && vino.pais === bandera))
+      })
+
+  }
+
+  else if (datoLimpio !== "" && arrayCheckbox.length == 0) {
+      vinosPorBanderas = arrayAFiltrar.filter(vino => vino.nombre.toLowerCase().includes(datoLimpio))
+  }
+
+  else if (datoLimpio === "" && arrayCheckbox.length > 0) {
+
+      arrayCheckbox.map(category =>
+          vinosPorBanderas.push(...arrayAFiltrar.filter(vino => vino.pais === category))
+      )
+  }
+
+  else {
+      vinosPorBanderas = arrayAFiltrar
+
+  }
+
+
+  vinosPorBanderas.length > 0 ?
+      pintarHTML(vinosPorBanderas) :
+      tarjetasVinos.innerHTML = `<h1 class="ceroResult" >No se encontraron eventos para tu busqueda </h1>`
+
+}
