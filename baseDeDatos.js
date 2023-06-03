c8cd4f6476beb0521f06498838964d968990c604
Scarlethev <scarleth_07@hotmail.com>
2023-06-02 21:45:55 -0300

    baseDeDatos

---
+++ baseDeDatos.js
@@ -0,0 +1,83 @@
+var dataGym = {
+
+    "productos": [
+      {
+        "id": 1,
+        "image": "imagenes/cuerdasParaSaltar.JPG",
+        "titulo": "Soga",
+        "categoria" : "aerobico",
+        "description": "Esta cuerda para saltar hará que mejores tu fuerza, resistencia, agilidad y coordinación con probablemente la mejor comba en el mercado. Es de gran calidad y duradera para la máxima satisfacción y seguridad durante el entrenamiento.",
+        "price": 5450
+      },
+      
+      {
+        "id": 2,
+        "image": "imagenes/man-g77843936b_1280.jpg",
+        "titulo": "Pesas 3KG",
+        "categoria" : "Pesas",
+        "description": "",
+        "price": 5700 
+      },
+
+      {
+        "id": 3,
+        "image": "imagenes/suplemento.JPG",
+        "titulo": "whey Protein",
+        "categoria" : "suplementos",
+        "description": "",
+        "price": 6600
+      },
+
+      {
+        "id": 4,
+        "image": "imagenes/termica-mc-51-fafefa47abe33e354b16729461259711-1024-1024.JPG",
+        "titulo": "Remera termica",
+        "categoria" : "indumentaria",
+        "description": "",
+        "price": 18000
+      },
+      {
+        "id": 5,
+        "image": "imagenes/short deportivo.jpg",
+        "titulo": "Short",
+        "categoria" : "indumentaria",
+        "description": "",
+        "price": 20000
+      },
+
+      {
+        "id": 6,
+        "image": "imagenes\zapatillasDeportivas.JPG",
+        "titulo": "Zapatillas",
+        "categoria" : "indumentaria",
+        "description": "Estas zapatillas reducen el esfuerzo de los músculos y de los tendones, tienen un buen agarre a las diferentes superficies del suelo en función del deporte.",
+        "price": 35000
+      },
+      {
+        "id": 7,
+        "image":"imagenes\setPlataforma.JPG",
+        "titulo": "Set Plataforma",
+        "categoria" : "aerobico",
+        "description": "Set Plataforma :CUSHION DE EQUILIBRIO en color azul ideal para la realización de ejercicios de propiocepción y equilibrio, fabricado en foam de alta densidad .Dimensiones: 48396 cm. Color: Azul real.",
+        "price": 16000
+      },
+
+      {
+        "id": 8,
+        "image":"imagenes\pesasRusas.JPG",
+        "titulo": "Pesas Relativas",
+        "categoria" : "pesas",
+        "description": "PESAS RUSAS:  producto Pesas rusas confeccionada en plástico, rellena de cal, cemento y arena. Son una gran herramienta de entrenamiento funcional.",
+        "price": 8500
+      },
+
+      {
+        "id": 9,
+        "image":"imagenes\suplementoWhey.JPG",
+        "titulo": "Suplemento Whey",
+        "categoria" : "suplementos",
+        "description": "Contiene todos los aminoacidos esenciales y no esenciales ya que son naturales del concentrado de proteina de suero lacteo",
+        "price": 4320
+      }
+    ]
+    }
